6d4b859545f747df001ef6db5e2ab406ed99c1ab
Arjun11210829 <arjun.18ee@kct.ac.in>
2022-04-03 00:46:24 +0530

    add files

---
+++ 3_Implementation/inc/ElectronicHelper.h
@@ -0,0 +1,21 @@
+#ifndef __ElectronicHelper_H__
+#define __ElectronicHelper_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+struct ElectronicHelper
+{
+	
+	int initial_option;
+	int vol_option;
+    int cur_option;
+	int res_option;
+	int pow_option;
+	int num_colors;
+};
+
+struct ElectronicHelper B;
+#endif
